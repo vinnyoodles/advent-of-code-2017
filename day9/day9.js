5207484985965a19890de0b216a5594fd74afd8e
Vincent Le <vinnyoodles@gmail.com>
2017-12-09 00:19:17 -0500

    Finish day 9 and create subdirs

---
+++ day9/day9.js
@@ -0,0 +1,44 @@
+var _ = require('underscore');
+var fs = require('fs');
+var filename = 'day9.txt';
+fs.readFile(filename, 'utf8', function(err, data) {
+  if (err) throw err;
+  _.each(data.split('\n'), solve);
+});
+
+function solve(data) {
+    var total = 0;
+    var level = 0;
+    var index = 0;
+    var count = 0;
+    var garbage = false;
+    while (index < data.length) {
+        var char = data[index];
+        if (char === '{' && !garbage) {
+            level ++;
+        } else if (char === '}' && !garbage) {
+            total += level;
+            level --;
+        } else if (char === '!') {
+            index += 2;
+            continue;
+        } else if (char === '>' && garbage) {
+            garbage = false;
+            index ++;
+            continue;
+        } else if (char === '<' && !garbage) {
+            garbage = true;
+            index ++;
+            continue;
+        }
+
+        if (garbage)
+            count ++;
+        index ++;
+    }
+    console.log(total, count);
+}
+
+
+function part2(data) {
+}
